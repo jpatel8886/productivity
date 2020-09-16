cd56f62024e6cf9f6fd7fb547542ee0e4a485dbc
jpatel8886 <jpatel34@wisc.edu>
2020-09-16 19:10:58 +0530

    Updates

---
+++ precache-manifest.db49b45374115392d1bef6c84691b577.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7724cbdebebfbafd8f516817e7731973",
+    "url": "/productivity/index.html"
+  },
+  {
+    "revision": "a202e528e3813e42de19",
+    "url": "/productivity/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "a5db4e441590bd1b02c0",
+    "url": "/productivity/static/css/main.446e7643.chunk.css"
+  },
+  {
+    "revision": "a202e528e3813e42de19",
+    "url": "/productivity/static/js/2.017d122f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/productivity/static/js/2.017d122f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5db4e441590bd1b02c0",
+    "url": "/productivity/static/js/main.b5b82257.chunk.js"
+  },
+  {
+    "revision": "009b7ef5872274e56ced",
+    "url": "/productivity/static/js/runtime-main.3abbd96c.js"
+  }
+]);
